3ac84aa8af691dc60457d0b6df11855824a5063d
Eldar Aliiev <bc.zim.green@gmail.com>
2018-07-28 19:57:40 +0300

    Docs configuration added

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+project = 'Coralogix Python SDK'
+github_project_url = 'https://github.com/coralogix/python-coralogix-sdk'
+copyright = '2018, Coralogix Ltd.'
+author = 'Coralogix Ltd.'
+version = '2.0.1'
+release = '2.0.1'
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = None
+exclude_patterns = ['_build']
+default_role = 'literal'
+pygments_style = 'sphinx'
+todo_include_todos = False
