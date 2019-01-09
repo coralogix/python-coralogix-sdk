5ac01f99e6a55a5896b1aeeb9b7930a90be494d5
Eldar Aliiev <bc.zim.green@gmail.com>
2019-01-09 20:17:37 +0200

    Version fixed, setup fixed

--- coralogix/__init__.py
+++ coralogix/__init__.py
@@ -10,10 +10,10 @@ Email: info@coralogix.com
 __name__ = 'coralogix_logger'
 __author__ = 'Coralogix Ltd.'
 __email__ = 'info@coralogix.com'
-__copyright__ = 'Copyright 2018, Coralogix Ltd.'
+__copyright__ = 'Copyright 2019, Coralogix Ltd.'
 __credits__ = ['Ariel Assaraf', 'Lior Redlus', 'Amnon Shahar']
 __license__ = 'Apache Version 2.0'
-__version__ = '2.0.1-6'
+__version__ = '2.0.1-7'
 __maintainer__ = 'Coralogix Ltd.'
-__date__ = '6 June 2018'
+__date__ = '9 January 2019'
 __status__ = 'Stable'
