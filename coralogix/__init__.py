9da520cf978323eb6e48fa6bd01a61de56ff1fbc
Eldar Aliiev <bc.zim.green@gmail.com>
2021-05-27 15:51:33 +0300

    Additional flush added

--- coralogix/__init__.py
+++ coralogix/__init__.py
@@ -10,10 +10,10 @@ Email: info@coralogix.com
 __name__ = 'coralogix_logger'
 __author__ = 'Coralogix Ltd.'
 __email__ = 'info@coralogix.com'
-__copyright__ = 'Copyright 2020, Coralogix Ltd.'
+__copyright__ = 'Copyright 2021, Coralogix Ltd.'
 __credits__ = ['Ariel Assaraf', 'Lior Redlus', 'Amnon Shahar']
 __license__ = 'Apache Version 2.0'
-__version__ = '2.0.4'
+__version__ = '2.0.5'
 __maintainer__ = 'Coralogix Ltd.'
-__date__ = '12 January 2021'
+__date__ = '27 May 2021'
 __status__ = 'Stable'
